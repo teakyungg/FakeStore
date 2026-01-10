13995d954d9a1b21bfbf81bb9df1a42c08fc6538
teakyungg <kak0502@naver.com>
2026-01-10 17:07:01 +0900

    feat/HeaderSearchBar 컴포넌트 적용

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import { HeaderSearchBar } from "./components/HeaderSearchBar/HeaderSearchBar";
+
 export default function Home() {
   return (
-    <div style={{ height: "1000px", backgroundColor: "lightblue" }}>
-      <div style={{ height: "100px", backgroundColor: "lightgray" }}>이거 만약에 헤더라고 한다면</div>
-      <nav style={{ position: "sticky", top: "50px", backgroundColor: "lightgreen" }}>position: "sticky"</nav>
+    <div style={{ height: "2000px" }}>
+      <HeaderSearchBar />
+      {/* <nav style={{ position: "sticky", top: "60px", backgroundColor: "lightgreen" }}>sticky</nav> */}
     </div>
   );
 }
