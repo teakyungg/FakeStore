e132a8a788e66d7b7e718aa67a529c93d1b3a144
teakyungg <kak0502@naver.com>
2026-01-11 18:55:55 +0900

    feat/CategoryNav 컴포넌트 페이지 적용

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import { CategoryNav } from "./components/CategoryNav/CategoryNav";
 import { HeaderSearchBar } from "./components/HeaderSearchBar/HeaderSearchBar";
 
 export default function Home() {
   return (
     <div style={{ height: "2000px" }}>
       <HeaderSearchBar />
-      {/* <nav style={{ position: "sticky", top: "60px", backgroundColor: "lightgreen" }}>sticky</nav> */}
+      <CategoryNav />
     </div>
   );
 }
