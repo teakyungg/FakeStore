dbc681f9925a96568be40ca9db05e693da1b053e
teakyungg <kak0502@naver.com>
2026-01-12 00:01:40 +0900

    refactor/Home 컴포넌트에서 사용하지 않는 코드 삭제

--- app/(page)/Home/Home.tsx
+++ app/(page)/Home/Home.tsx
@@ -3,7 +3,6 @@
 import { CategoryNav } from "@/app/components/CategoryNav/CategoryNav";
 import { HeaderSearchBar } from "@/app/components/HeaderSearchBar/HeaderSearchBar";
 import { MainMenu } from "@/app/(page)/Home/components/MainMenu/MainMenu";
-import { useState } from "react";
 
 export function Home() {
   return (
