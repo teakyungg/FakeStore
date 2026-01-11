6d924ffe6968e819ec9752c5a62c2b2f195341b9
teakyungg <kak0502@naver.com>
2026-01-11 23:56:39 +0900

    feat: 카테고리 클릭 시 해당 목록 표시 및 변경된 카테고리 상태를 zustand로 관리

--- app/components/CategoryNav/CategoryNav.tsx
+++ app/components/CategoryNav/CategoryNav.tsx
@@ -2,16 +2,23 @@
 
 import { useState } from "react";
 import styles from "./CategoryNav.module.scss";
+import { useCategoryStore } from "@/app/useCategoryStore";
 
 export function CategoryNav() {
   const [itemLine, setItemLine] = useState(0);
   const categoryMenu = ["전체", "추천", "랭킹", "세일"];
+  const setNowCategory = useCategoryStore((state) => state.setNowCategory);
+
+  const setCategory = (value: string, index: number) => {
+    setNowCategory(value);
+    setItemLine(index);
+  };
 
   return (
     <nav className={`${styles.categoryNav} ${styles.inner}`}>
       <ul className={styles.categoryMenu}>
         {categoryMenu.map((value, index) => (
-          <li className={styles.item} key={value} onClick={() => setItemLine(index)}>
+          <li className={styles.item} key={value} onClick={() => setCategory(value, index)}>
             {value}
             <div className={itemLine === index ? styles.itemLineOn : ""}></div>
           </li>
