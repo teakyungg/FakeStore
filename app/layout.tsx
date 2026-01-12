77f5f16ccf82e209b9139efb4a66dc8792ee51ad
teakyungg <kak0502@naver.com>
2026-01-12 18:28:55 +0900

    feat/Footer 컴포넌트 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.scss";
 import { Header } from "./components/Header/Header";
+import { Footer } from "./components/Footer/Footer";
 
 export const metadata: Metadata = {
   title: "FakeStore",
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
