187fc644f85a897252aa0f2a5480074ab4dd85b1
teakyungg <kak0502@naver.com>
2026-01-12 22:28:50 +0900

    feat/ProductCard 이미지 스켈레톤 UI 추가

--- app/components/ProductCard/ProductCard.tsx
+++ app/components/ProductCard/ProductCard.tsx
@@ -1,4 +1,7 @@
+import { useEffect, useState } from "react";
 import styles from "./ProductCard.module.scss";
+import imagesLoaded from "imagesloaded";
+import ClipLoader from "react-spinners/ClipLoader";
 
 interface ProductCardType {
   imgurl: string;
@@ -8,8 +11,17 @@ interface ProductCardType {
 }
 
 export function ProductCard({ imgurl, brand, title, price }: ProductCardType) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    imagesLoaded(`.${styles.productImage}`, { background: true }, function () {
+      setLoading(false);
+    });
+  }, []);
+
   return (
     <article className={styles.productCard}>
+      <div className={styles.loading} style={{ opacity: loading ? 1 : 0 }}></div>
       <div className={styles.productImage} style={{ backgroundImage: `url(${imgurl})` }}></div>
 
       <div className={styles.productDirBox}>
