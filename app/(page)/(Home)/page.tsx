ce33b619760cc52977da093c5e7279a2ea5f4764
teakyungg <kak0502@naver.com>
2026-01-16 22:11:27 +0900

    feat/상품 데이터 호출 위치 컴포넌트 변경

--- app/(page)/(Home)/page.tsx
+++ app/(page)/(Home)/page.tsx
@@ -1,13 +1,58 @@
+"use client";
+
 import { CategoryNav } from "@/app/components/CategoryNav/CategoryNav";
 import { HeaderSearchBar } from "@/app/components/HeaderSearchBar/HeaderSearchBar";
-import { MainMenu } from "@/app/(page)/(Home)/components/MainMenu/MainMenu";
+import { ProductCategorySection } from "@/app/components/ProductCategorySection/ProductCategorySection";
+import { useEffect, useState } from "react";
+
+const API_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+export interface productsType {
+  id: string;
+  brand: string;
+  title: string;
+  description: string;
+  price: string;
+  images: string[];
+}
 
 export default function Main() {
+  const [BeautyProduct, setBeautyProduct] = useState<productsType[]>([]);
+  const [FragrancesProduct, setFragrancesProduct] = useState<productsType[]>([]);
+  const [GroceriesProduct, setGroceriesProduct] = useState<productsType[]>([]);
+
+  useEffect(() => {
+    const fetchProducts = async (category: string, setBeautyProduct: (products: productsType[]) => void) => {
+      try {
+        const res = await fetch(`${API_URL}/products/category/${category}`);
+
+        if (!res.ok) {
+          throw new Error("상품 리스트 불러오기 실패");
+        }
+
+        const data = await res.json();
+        const productData: productsType[] = data.products;
+
+        setBeautyProduct(productData);
+      } catch {
+        alert("잠시 후 다시 시도해 주세요.");
+      }
+    };
+
+    fetchProducts("Beauty", setBeautyProduct);
+    fetchProducts("Fragrances", setFragrancesProduct);
+    fetchProducts("Groceries", setGroceriesProduct);
+  }, []);
+
   return (
     <>
       <HeaderSearchBar />
       <CategoryNav />
-      <MainMenu />
+      <main>
+        <ProductCategorySection categroy={"Beauty"} products={BeautyProduct} />
+        <ProductCategorySection categroy={"Fragrances"} products={FragrancesProduct} />
+        <ProductCategorySection categroy={"Groceries"} products={GroceriesProduct} rowLength={2} />
+      </main>
     </>
   );
 }
--- app/components/ProductCategorySection/ProductCategorySection.tsx
+++ app/components/ProductCategorySection/ProductCategorySection.tsx
@@ -1,48 +1,17 @@
-"use client";
-
 import { ProductCard } from "@/app/components/ProductCard/ProductCard";
 import styles from "./ProductCategorySection.module.scss";
-import { useEffect, useState } from "react";
 import { ProductLoadingCard } from "@/app/components/ProductLoadingCard/ProductLoadingCard";
 import ScrollContainer from "react-indiana-drag-scroll";
-
-const API_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
-
-interface productsType {
-  id: string;
-  brand: string;
-  title: string;
-  description: string;
-  price: string;
-  images: string[];
-}
+import { productsType } from "@/app/(page)/(Home)/page";
 
 interface ProductCategorySectionType {
+  products: productsType[];
   categroy: string;
   rowLength?: number; // 세로행 갯수
 }
 
-export function ProductCategorySection({ categroy, rowLength = 1 }: ProductCategorySectionType) {
-  const [products, setProducts] = useState<productsType[]>([]);
+export function ProductCategorySection({ categroy, rowLength = 1, products }: ProductCategorySectionType) {
   const loadingCardLength = 10; // 로딩중 보일 컴포넌트 갯수
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const res = await fetch(`${API_URL}/products/category/${categroy}?limit=${20}`);
-
-        if (!res.ok) {
-          throw new Error("상품 리스트 불러오기 실패");
-        }
-
-        const data = await res.json();
-        setProducts(data.products);
-      } catch {
-        alert("잠시 후 다시 시도해 주세요.");
-      }
-    };
-    fetchProducts();
-  }, []);
 
   return (
     <div className={styles.productCategorySection}>
