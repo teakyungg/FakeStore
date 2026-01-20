899282e0829ad6b6a6068ad2fc41f6fadb7b6c69
teakyungg <kak0502@naver.com>
2026-01-20 22:21:01 +0900

    feat/물건 상세 페이지 마크업

--- app/(page)/[Products]/page.tsx
+++ app/(page)/[Products]/page.tsx
@@ -1,8 +1,23 @@
 export default function ProductItem() {
   /* 
-    1. 물건 제목
-    2. 물건 가격
+    1. 헤더
+    2. 물건 제목
+    3. 물건 가격
     
   */
-  return <div>물건 상세 페이지 제작하기</div>;
+  return (
+    <div>
+      {/* inner */}
+      <div>
+        <div>상품이미지</div>
+        {/* 상품 설명칸 */}
+        <div>
+          <div>상품 이름</div>
+          <div>상품 가격</div>
+          <div>장바구니</div>
+          <div>결제</div>
+        </div>
+      </div>
+    </div>
+  );
 }
