33293792cfc0397af66ee25b2990d64920864796
teakyungg <kak0502@naver.com>
2026-01-25 22:35:27 +0900

    feat/ProductPrice 로딩시 사용자 눈에 안보이도록 조건문 추가

--- app/components/ProductPrice/ProductPrice.tsx
+++ app/components/ProductPrice/ProductPrice.tsx
@@ -9,17 +9,19 @@ interface ProductPriceType {
 }
 
 export function ProductPrice({ price = 0, discount = 0 }: ProductPriceType) {
+  const finalPrice = Number((price * (1 - discount / 100)).toFixed(2));
+
   return (
     <div className={styles.productPrice}>
       {/* 원래 가격 */}
-      {price && <s className={styles.price}>{price}$</s>}
+      {price > 0 && <s className={styles.price}>{price}$</s>}
 
       <div className={styles.priceBox}>
         {/* 할인률 */}
         {discount > 0 && <p className={styles.discount}>{discount}%</p>}
 
         {/* 최종 가격 */}
-        <p className={styles.finalPrice}>{(price * (1 - discount / 100)).toFixed(2)}$</p>
+        {finalPrice > 0 && <p className={styles.finalPrice}>{finalPrice}$</p>}
       </div>
     </div>
   );
